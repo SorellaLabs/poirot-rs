6d30d19ade923ee3c96c75087599ece98de43009
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-07-01 20:03:35 +0200

    test

--- poirot-core/src/main.rs
+++ poirot-core/src/main.rs
@@ -25,7 +25,10 @@ async fn main() {
 
 async fn run() -> Result<(), Box<dyn Error>> {
     // Read environment variables
-    let db_path = env::var("DB_PATH").expect("DB_PATH is not set in env");
+    let db_path = match env::var("DB_PATH") {
+        Ok(path) => path,
+        Err(_) => return Err(Box::new(std::env::VarError::NotPresent)),
+    };
     let db_path = Path::new(&db_path);
 
     // Get a handle to the current runtime.
@@ -37,12 +40,24 @@ async fn run() -> Result<(), Box<dyn Error>> {
     // Trace this mev block:
     let block_number = BlockId::from(17600791);
 
-    let block = tracer.reth_api.block_transaction_count_by_number(17600791.into()).await?;
+    let block = match tracer.reth_api.block_transaction_count_by_number(17600791.into()).await {
+        Ok(block) => block,
+        Err(e) => {
+            eprintln!("Failed to get block transaction count: {:?}", e);
+            return Err(Box::new(e))
+        }
+    };
 
     println!("Block: {:?}", block.unwrap());
 
     let tracing_opt = GethDebugTracingOptions::default();
-    let block_traces1 = tracer.reth_debug.debug_trace_block(block_number, tracing_opt).await?;
+    let block_traces1 = match tracer.reth_debug.debug_trace_block(block_number, tracing_opt).await {
+        Ok(block_traces) => block_traces,
+        Err(e) => {
+            eprintln!("Failed to trace block: {:?}", e);
+            return Err(Box::new(e))
+        }
+    };
 
     // Print traces
     for trace in block_traces1 {
