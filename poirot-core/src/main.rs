0df4a3708a14db3ebed308e93a3dda3463ca6589
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-07-01 02:21:12 +0200

    fixed runtime

--- poirot-core/src/main.rs
+++ poirot-core/src/main.rs
@@ -7,15 +7,14 @@ use poirot_rs::TracingClient;
 use reth_primitives::BlockId;
 use reth_rpc_types::trace::geth;
 
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let rt = tokio::runtime::Runtime::new()?;
-    let handle = rt.handle().clone();
-
     // Read environment variables
     let db_path = env::var("DB_PATH").expect("DB_PATH is not set in env");
     let db_path = Path::new(&db_path);
+
+    // Get a handle to the current runtime.
+    let handle = tokio::runtime::Handle::current();
 
     // Initialize TracingClient
     let tracer = TracingClient::new(&db_path, handle);
@@ -28,12 +27,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for trace in block_traces {
         println!("{:?}", trace);
     }
-    /* 
-    let block_traces = tracer.reth_debug.debug_trace_block(block_number).await?;
-
-    for trace in block_traces {
-        println!("{:?}", trace);
-    } */
 
     Ok(())
 }
