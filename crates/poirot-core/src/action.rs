1140a5c2217cbf7a9ce92c4a1e646de9ac60c73c
ts0yu <ts0yu@proton.me>
2023-07-06 13:23:21 +0100

    Update action.rs

--- crates/poirot-core/src/action.rs
+++ crates/poirot-core/src/action.rs
@@ -1,6 +1,6 @@
 use reth_primitives::{Bytes, H256, U256};
 use reth_rpc_types::trace::parity::LocalizedTransactionTrace;
-use alloy_primitives::H160;
+use reth_primitives::H160;
 
 #[derive(Debug, Clone)]
 pub struct Action {
