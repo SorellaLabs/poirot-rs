0d5feb7a45453ea01be82af755c5a07d8e3767cf
ts0yu <ts0yu@proton.me>
2023-07-06 16:30:39 +0100

    a

--- crates/poirot-core/src/parser.rs
+++ crates/poirot-core/src/parser.rs
@@ -63,24 +63,32 @@ impl Parser {
                     Err(_) => return None,
                 };
 
-                // println!("{decoded:#?}");
-
                 match decoded {
                     IERC20::IERC20Calls::transfer(transfer_call) => {
-                        // println!("{transfer_call:#?}");
                         let transfer = Transfer {
                             to: transfer_call.to,
                             amount: transfer_call.amount.into(),
                             token: call.to,
                         };
+
+                        return Some(Action {
+                            ty: ActionType::Transfer(transfer),
+                            hash: curr.transaction_hash.unwrap(),
+                            block: curr.block_number.unwrap(),
+                        })
                     }
                     IERC20::IERC20Calls::transferFrom(transfer_from_call) => {
-                        println!("{transfer_from_call:#?}");
                         let transfer = Transfer {
                             to: transfer_from_call.to,
                             amount: transfer_from_call.amount.into(),
                             token: call.to,
                         };
+
+                        return Some(Action {
+                            ty: ActionType::Transfer(transfer),
+                            hash: curr.transaction_hash.unwrap(),
+                            block: curr.block_number.unwrap(),
+                        })
                     }
                     _ => return None,
                 }
