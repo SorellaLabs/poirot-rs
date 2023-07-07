eab56f1e03545a9ffa7e48e50a2bce697e7137ad
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-07-07 20:19:44 +0200

    change types to alloy U256 & created array of deposits

--- crates/poirot-core/src/action.rs
+++ crates/poirot-core/src/action.rs
@@ -1,5 +1,5 @@
-use alloy_primitives::Address;
-use reth_primitives::{Bytes, H160, H256, U256};
+use alloy_primitives::{Address, U256};
+use reth_primitives::{Bytes, H160, H256};
 use reth_revm::precompile::primitives::ruint::Uint;
 use reth_rpc_types::trace::parity::LocalizedTransactionTrace;
 
@@ -13,77 +13,73 @@ pub struct Action {
 #[derive(Debug, Clone)]
 pub enum ActionType {
     Transfer(Transfer),
-
     PoolCreation(PoolCreation),
     Swap(Swap),
-
     WethDeposit(Deposit),
     WethWithdraw(Withdrawal),
-
     Unclassified(LocalizedTransactionTrace),
-    None,
 }
 
 #[derive(Debug, Clone)]
 pub struct Withdrawal {
     pub to: H160,
-    pub amount: alloy_primitives::Uint<256, 4>,
+    pub amount: U256,
 }
 
 #[derive(Debug, Clone)]
 pub struct Deposit {
     pub from: H160,
-    pub amount: Uint<256, 4>,
+    pub tokens: Vec<H160>,
+    pub amounts: Vec<U256>,
 }
 
 #[derive(Debug, Clone)]
 pub struct Transfer {
-    // pub from: Address,
-    pub to: Address,
-    pub amount: ruint2::Uint<256, 4>,
+    pub to: H160,
+    pub amount: U256,
     pub token: H160,
 }
 
 #[derive(Debug, Clone)]
 pub struct PoolCreation {
-    pub token_0: Address,
-    pub token_1: Address,
+    pub token_0: H160,
+    pub token_1: H160,
     pub fee: u32,
 }
 
 #[derive(Debug, Clone)]
 pub struct Swap {
-    pub recipient: Address,
+    pub recipient: H160,
     pub direction: bool,
-    pub amount_specified: alloy_primitives::Signed<256, 4>,
-    pub price_limit: alloy_primitives::Uint<256, 4>,
+    pub amount_specified: U256,
+    pub price_limit: U256,
     pub data: Vec<u8>,
 }
 
 impl Transfer {
     /// Public constructor function to instantiate a [`Transfer`].
-    pub fn new(to: Address, amount: ruint2::Uint<256, 4>, token: H160) -> Self {
+    pub fn new(to: H160, amount: U256, token: H160) -> Self {
         Self { to, amount, token }
     }
 }
 
 impl PoolCreation {
     /// Public constructor function to instantiate a [`PoolCreation`].
-    pub fn new(token_0: Address, token_1: Address, fee: u32) -> Self {
+    pub fn new(token_0: H160, token_1: H160, fee: u32) -> Self {
         Self { token_0, token_1, fee }
     }
 }
 
 impl Deposit {
     /// Public constructor function to instantiate a [`Deposit`].
-    pub fn new(from: H160, amount: Uint<256, 4>) -> Self {
-        Self { from, amount }
+    pub fn new(from: H160, tokens: Vec<H160>, amounts: Vec<U256>) -> Self {
+        Self { from, tokens, amounts }
     }
 }
 
 impl Withdrawal {
     /// Public constructor function to instantiate a [`Withdrawal`].
-    pub fn new(to: H160, amount: alloy_primitives::Uint<256, 4>) -> Self {
+    pub fn new(to: H160, amount: U256) -> Self {
         Self { to, amount }
     }
 }
