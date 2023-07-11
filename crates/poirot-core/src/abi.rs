bd9ebebd8385cb10ac087bc626b3c0a886acf395
0xvanbeethoven <vanbeethoven@sorellacapital.com>
2023-07-11 05:19:45 +0200

    pseudo code, could never work, need dyn-abi

--- crates/poirot-core/src/abi.rs
+++ crates/poirot-core/src/abi.rs
@@ -1,112 +1,77 @@
-use serde_json;
-use std::{env, fs, path::Path};
+use std::collections::HashMap;
+use std::path::PathBuf;
+use alloy_sol_types::decode;
+use alloy_json_abi::Function;
+use serde_json::Value;
+use reth_rpc_types::trace::parity::{Action as RethAction, LocalizedTransactionTrace};
+use revm_primitives::bits::B160;
 
-use alloy_json_abi::{AbiItem, Error, JsonAbi, Param};
+pub struct ContractAbiStorage<'a> {
+    mapping: HashMap<&'a B160, PathBuf>,
+}
 
-pub fn load_all_jsonabis(relative_path: &str) -> Result<Vec<JsonAbi>, Box<dyn std::error::Error>> {
-    let mut abis = Vec::new();
-
-    let current_dir = env::current_dir()?;
-    let dir_path = current_dir.join(relative_path);
-
-    if dir_path.is_dir() {
-        for entry in fs::read_dir(dir_path)? {
-            let entry = entry?;
-            let path = entry.path();
-
-            // Ensure the file is a .json file
-            if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("json") {
-                let content = fs::read_to_string(&path)?;
-                let json_abi: JsonAbi = serde_json::from_str(&content)?;
-                abis.push(json_abi);
-            }
+impl<'a> ContractAbiStorage<'a> {
+    pub fn new() -> Self {
+        Self {
+            mapping: HashMap::new(),
         }
     }
 
-    Ok(abis)
+    pub fn add_abi(&mut self, contract_address: &'a B160, abi_path: PathBuf) {
+        self.mapping.insert(contract_address, abi_path);
+    }
+
+    pub fn get_abi(&self, contract_address: &'a B160) -> Option<&PathBuf> {
+        self.mapping.get(contract_address)
+    }
 }
 
+pub fn sleuth<'a>(
+    storage: &'a ContractAbiStorage,
+    trace: LocalizedTransactionTrace,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let action = trace.trace.action;
+
+    let (contract_address, input) = match action {
+        RethAction::Call(call_action) => (call_action.to, call_action.input.to_vec()),
+        _ => return Err(From::from("The action in the transaction trace is not Call(CallAction)")),
+    };
+
+    let contract_address = contract_address;
+    let abi_path = storage
+        .get_abi(&contract_address)
+        .ok_or("No ABI found for this contract")?;
 
 
-#[test]
-fn complex_error() {
-    let json = r#"{
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "reason",
-                "type": "string"
-            }
-        ],
-        "name": "SomeName",
-        "type": "error"
-    }"#;
-    let decoded: Error = serde_json::from_str(json).unwrap();
-    assert_eq!(
-        decoded,
-        Error {
-            inputs: vec![Param {
-                internal_type: Some("string".into()),
-                name: "reason".into(),
-                ty: "string".into(),
-                components: vec![],
-            }],
-            name: "SomeName".into()
+    // Read the JSON ABI file
+    let file = std::fs::File::open(abi_path)?;
+    let reader = std::io::BufReader::new(file);
+    
+    // Parse the JSON ABI
+    let json_abi: Value = serde_json::from_reader(reader)?;
+
+    // Extract function selectors from the ABI
+    let mut function_selectors = HashMap::new();
+    if let Value::Array(functions) = json_abi.get("functions").unwrap() {
+        for function in functions {
+            let function: Function = serde_json::from_value(function.clone())?;
+            function_selectors.insert(function.selector(), function);
         }
-    );
-}
+    }
 
-macro_rules! abi_parse_tests {
-    ($($name:ident($path:literal, $len:literal))*) => {$(
-        #[test]
-        fn $name() {
-            parse_test(include_str!($path), $len);
-        }
-    )*};
-}
+    // Extract the function selector from the input
+    let function_selector = &input[..4];
 
-abi_parse_tests! {
-    abiencoderv2("abi/Abiencoderv2Test.json", 1)
-    console("abi/console.json", 379)
-    event_with_struct("abi/EventWithStruct.json", 1)
-    large_array("abi/LargeArray.json", 1)
-    large_struct("abi/LargeStruct.json", 1)
-    large_structs("abi/LargeStructs.json", 4)
-    large_tuple("abi/LargeTuple.json", 1)
-    seaport("abi/Seaport.json", 69)
-}
+    // Find the matching function in the ABI
+    let function = function_selectors
+        .get(function_selector)
+        .ok_or("No matching function found in the ABI")?;
 
-fn parse_test(s: &str, len: usize) {
-    let abi_items: Vec<AbiItem<'_>> = serde_json::from_str(s).unwrap();
-    assert_eq!(abi_items.len(), len);
+    // Decode the input data
+    let decoded = decode::<Vec<Value>>(&input, true)?;
 
-    let json = serde_json::to_string(&abi_items).unwrap();
-    let abi1: JsonAbi = serde_json::from_str(&json).unwrap();
+    // Convert the decoded input to a string for printing
+    let printout = format!("{:?}", decoded);
 
-    let abi2: JsonAbi = serde_json::from_str(s).unwrap();
-
-    assert_eq!(len, abi2.len());
-    assert_eq!(abi1, abi2);
-
-    let json = serde_json::to_string(&abi2).unwrap();
-    let abi3: JsonAbi = serde_json::from_str(&json).unwrap();
-    assert_eq!(abi2, abi3);
-
-    iterator_test(abi1.items(), abi1.items().rev(), len);
-    iterator_test(abi1.items().skip(1), abi1.items().skip(1).rev(), len - 1);
-    iterator_test(abi1.clone().into_items(), abi1.into_items().rev(), len);
-}
-
-fn iterator_test<T, I, R>(items: I, rev: R, len: usize)
-where
-    T: PartialEq + std::fmt::Debug,
-    I: Iterator<Item = T> + DoubleEndedIterator + ExactSizeIterator,
-    R: Iterator<Item = T>,
-{
-    assert_eq!(items.len(), len);
-    assert_eq!(items.size_hint(), (len, Some(len)));
-
-    let mut items2: Vec<_> = items.collect();
-    items2.reverse();
-    assert_eq!(items2, rev.collect::<Vec<_>>());
+    Ok(printout)
 }
